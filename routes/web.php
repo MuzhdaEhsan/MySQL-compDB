c050f53279ddc85fd8121e2131bf96a114bee6d8
Tan Vu <duytanvu@icloud.com>
2022-01-05 14:12:07 -0500

    Create CheckIsAdmin middleware

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Http\Controllers\AttributeController;
+use App\Http\Controllers\CompetencyController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KnowledgeController;
+use App\Http\Controllers\LogController;
+use App\Http\Controllers\SkillController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +27,14 @@ Auth::routes(['register' => false]);
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/', [HomeController::class, 'index']);
+    Route::resource('competencies', CompetencyController::class);
+    Route::resource('attributes', AttributeController::class);
+    Route::resource('skills', SkillController::class);
+    Route::resource('knowledge', KnowledgeController::class);
+    Route::resource('courses', CourseController::class);
+
+    Route::middleware(['admin'])->group(function () {
+        Route::resource('users', UserController::class);
+        Route::resource('logs', LogController::class);
+    });
 });
